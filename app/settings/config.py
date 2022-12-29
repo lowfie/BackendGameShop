2a71b2b90d100e69d14d5d45a996af5d42db02fc
Andrew Veselov <vesell4123@mail.ru>
2022-12-29 16:36:26 +0300

    add environs

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,1 +1,14 @@
-RELOAD = True
+import environs
+
+env = environs.Env()
+env.read_env()
+
+USER_POSTGRES = env.str('USER_POSTGRES')
+PASSWORD_POSTGRES = env.str('PASSWORD_POSTGRES')
+HOST_POSTGRES = env.str('HOST_POSTGRES')
+PORT_POSTGRES = env.str('PORT_POSTGRES')
+DATABASE_POSTGRES = env.str('DATABASE_POSTGRES')
+
+UVICORN_RELOAD = True
+
+
