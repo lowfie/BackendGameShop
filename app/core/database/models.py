be70f4f106e8229f868a961a8e6b17156363c685
Andrew Veselov <vesell4123@mail.ru>
2023-01-03 16:13:28 +0300

    create start database models

--- app/core/database/models.py
+++ app/core/database/models.py
@@ -1,4 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text,
+    DECIMAL,
+    Float,
+    String,
+    ForeignKey,
+    DateTime
+)
 
 from app.core.database.init import Base
 
@@ -9,8 +18,47 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     first_name = Column(String(30), nullable=True)
     last_name = Column(String(30), nullable=True)
-    nickname = Column(String(30), nullable=True)
+    nickname = Column(String(30), nullable=False)
     phone = Column(String(20), nullable=True, unique=True)
     email = Column(String(20), nullable=True, unique=True)
-    date_of_registry = Column(DateTime, nullable=False)
+    date_of_registry = Column(DateTime, nullable=True)
 
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String(20), nullable=True)
+    description = Column(Text, nullable=True)
+    price = Column(Float, nullable=True)
+    discount = Column(DECIMAL(3, 2), nullable=True)
+    image_path = Column(Text, nullable=True)
+    start_date = Column(DateTime, nullable=True)
+
+
+class UserGames(Base):
+    __tablename__ = 'user_games'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
+
+
+class Cart(Base):
+    __tablename__ = 'cart'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
+    price = Column(Float, nullable=True)
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
+    title = Column(String(50), nullable=False)
+    text = Column(Text, nullable=False)
+    evaluation = Column(Float, nullable=False, default=1)
