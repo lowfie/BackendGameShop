16b8261ac1eb8c539a0e905ac6f733bc9fe4fe48
Andrew Veselov <vesell4123@mail.ru>
2023-01-14 17:22:17 +0300

    create base models

--- app/core/database/models.py
+++ app/core/database/models.py
@@ -6,59 +6,64 @@ from sqlalchemy import (
     Float,
     String,
     ForeignKey,
-    DateTime
+    DateTime,
+    Boolean
 )
 
 from app.core.database.init import Base
+from fastapi_users.db import SQLAlchemyBaseUserTable
 
 
-class User(Base):
+class User(Base, SQLAlchemyBaseUserTable[int]):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    first_name = Column(String(30), nullable=True)
-    last_name = Column(String(30), nullable=True)
-    nickname = Column(String(30), nullable=False)
-    phone = Column(String(20), nullable=True, unique=True)
-    email = Column(String(20), nullable=True, unique=True)
+    id: int = Column(Integer, primary_key=True, nullable=False)
+    first_name: str = Column(String(30), nullable=True)
+    last_name: str = Column(String(30), nullable=True)
+    nickname: str = Column(String(30), nullable=True)
+    phone: str = Column(String(20), nullable=True, unique=True)
+    email: str = Column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password: str = Column(String(length=1024), nullable=False)
     date_of_registry = Column(DateTime, nullable=True)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
 
 
 class Game(Base):
     __tablename__ = 'games'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String(20), nullable=True)
-    description = Column(Text, nullable=True)
-    price = Column(Float, nullable=True)
-    discount = Column(DECIMAL(3, 2), nullable=True)
-    image_path = Column(Text, nullable=True)
+    id: int = Column(Integer, primary_key=True, nullable=False)
+    title: str = Column(String(20), nullable=True)
+    description: str = Column(Text, nullable=True)
+    price: float = Column(Float, nullable=True)
+    discount: float = Column(DECIMAL(3, 2), nullable=True)
+    image_path: str = Column(Text, nullable=True)
     start_date = Column(DateTime, nullable=True)
 
 
 class UserGames(Base):
     __tablename__ = 'user_games'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
+    id: int = Column(Integer, primary_key=True, nullable=False)
+    user_id: int = Column(Integer, ForeignKey('users.id'), nullable=False)
+    game_id: int = Column(Integer, ForeignKey('games.id'), nullable=False)
 
 
 class Cart(Base):
     __tablename__ = 'cart'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
-    price = Column(Float, nullable=True)
+    id: int = Column(Integer, primary_key=True, nullable=False)
+    user_id: int = Column(Integer, ForeignKey('users.id'), nullable=False)
+    game_id: int = Column(Integer, ForeignKey('games.id'), nullable=False)
 
 
 class Review(Base):
     __tablename__ = 'reviews'
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    game_id = Column(Integer, ForeignKey('games.id'), nullable=False)
-    title = Column(String(50), nullable=False)
-    text = Column(Text, nullable=False)
-    evaluation = Column(Float, nullable=False, default=1)
+    id: int = Column(Integer, primary_key=True, nullable=False)
+    user_id: int = Column(Integer, ForeignKey('users.id'), nullable=False)
+    game_id: int = Column(Integer, ForeignKey('games.id'), nullable=False)
+    title: str = Column(String(50), nullable=False)
+    text: str = Column(Text, nullable=False)
+    evaluation: float = Column(Float, nullable=False, default=1)
