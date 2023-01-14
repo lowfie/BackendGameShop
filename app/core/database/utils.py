be2f9442d6b6ca1a9047892bd4c64a39c12e7c4e
Andrew Veselov <vesell4123@mail.ru>
2023-01-14 17:30:59 +0300

    add service func get_user_db from user_manager

--- app/core/database/utils.py
+++ app/core/database/utils.py
@@ -1,6 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase
+
+from app.core.database.models import User
 from app.core.database.init import async_engine
 
 
@@ -8,3 +12,7 @@ async def get_session() -> AsyncSession:
     async_session = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_session)):
+    yield SQLAlchemyUserDatabase(session, User)
