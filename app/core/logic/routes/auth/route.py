dad3002f851c3b5bf8d12fc82bf6c0845f2deeeb
Andrew Veselov <vesell4123@mail.ru>
2023-01-16 01:17:18 +0300

    move change_users router

--- app/core/logic/routes/auth/route.py
+++ app/core/logic/routes/auth/route.py
@@ -4,9 +4,7 @@ from fastapi_users import FastAPIUsers
 from app.core.database.models import User
 from app.core.logic.routes.auth.auth_backend import auth_backend
 from app.core.logic.routes.auth.user_manager import get_user_manager
-from app.core.schemas.auth_shm import UserRead, RegisterIn, UserUpdate
-
-from app.core.logic.routes.users.route import users
+from app.core.schemas.auth_shm import UserRead, RegisterIn
 
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
@@ -25,10 +23,4 @@ auth_route.include_router(
     fastapi_users.get_register_router(UserRead, RegisterIn),
     prefix="/auth",
     tags=["auth"],
-)
-
-users.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
 )
--- app/core/logic/routes/users/route.py
+++ app/core/logic/routes/users/route.py
@@ -1,4 +1,14 @@
 from fastapi import APIRouter
 
+from app.core.logic.routes.auth.route import fastapi_users
+from app.core.schemas.auth_shm import UserRead, UserUpdate
+
+
 users = APIRouter()
+
+users.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
 
