0460d21573414f8d9d6a00edb446ca469cd00fbe
Andrew Veselov <vesell4123@mail.ru>
2023-01-20 11:12:15 +0300

    registration game route

--- app/core/logic/routes/reg_routers.py
+++ app/core/logic/routes/reg_routers.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.core.logic.routes.auth.route import auth_route
 from app.core.logic.routes.users.route import users
+from app.core.logic.routes.games.route import games
 
 api_router = APIRouter()
 
 api_router.include_router(auth_route)
 api_router.include_router(users, tags=['users'])
+api_router.include_router(games, tags=['games'])
