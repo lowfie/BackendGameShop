16cee32a5a965118b27cc125e06d180a3e31d2cd
Andrew Veselov <vesell4123@mail.ru>
2023-01-23 08:21:52 +0300

    register cart router

--- app/core/logic/routes/reg_routers.py
+++ app/core/logic/routes/reg_routers.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from app.core.logic.routes.auth.route import auth_route
 from app.core.logic.routes.users.route import users
 from app.core.logic.routes.games.route import games
+from app.core.logic.routes.cart.route import cart
 
 api_router = APIRouter()
 
 api_router.include_router(auth_route)
 api_router.include_router(users, tags=['users'])
 api_router.include_router(games, tags=['games'])
+api_router.include_router(cart, tags=['cart'])
