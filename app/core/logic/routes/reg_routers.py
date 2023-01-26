2e1c9e4635154fdc2b29a518965eea60aef85b67
Andrew Veselov <vesell4123@mail.ru>
2023-01-26 18:00:30 +0300

    added a fake balance replenishment system

--- app/core/logic/routes/reg_routers.py
+++ app/core/logic/routes/reg_routers.py
@@ -4,6 +4,8 @@ from app.core.logic.routes.auth.route import auth_route
 from app.core.logic.routes.users.route import users
 from app.core.logic.routes.games.route import games
 from app.core.logic.routes.cart.route import cart
+from app.core.logic.routes.economy.route import economy
+
 
 api_router = APIRouter()
 
@@ -11,3 +13,5 @@ api_router.include_router(auth_route)
 api_router.include_router(users, tags=['users'])
 api_router.include_router(games, tags=['games'])
 api_router.include_router(cart, tags=['cart'])
+api_router.include_router(economy, tags=['economy'])
+
