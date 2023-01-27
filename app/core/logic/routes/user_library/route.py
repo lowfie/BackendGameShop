446b420887e574685918a48bb527b980310c796e
Andrew Veselov <vesell4123@mail.ru>
2023-01-27 11:18:05 +0300

    displaying the game library

---
+++ app/core/logic/routes/user_library/route.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+from app.core.logic.routes.auth.route import fastapi_users
+from app.core.database.models import User, UserGames, Game
+from app.core.database.utils import get_session
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+library = APIRouter()
+current_user = fastapi_users.current_user()
+
+
+@library.get('/my_library/')
+async def deposit_balance(session: AsyncSession = Depends(get_session),
+                          user: User = Depends(current_user)):
+    library_games = (await session.execute(
+        select(Game.title,
+               Game.description,
+               Game.price,
+               Game.discount,
+               Game.image_path,
+               Game.start_date)
+        .join(UserGames)
+    )).all()
+    library_games = jsonable_encoder(library_games)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={'result': library_games})
