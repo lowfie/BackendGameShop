2cb58b9a33dad7520db4eb489c543b03e896e3fe
Andrew Veselov <vesell4123@mail.ru>
2023-01-27 09:12:22 +0300

    move async_session to init module

--- app/core/database/init.py
+++ app/core/database/init.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+
 from app.settings.config import (
     USER_POSTGRES,
     PASSWORD_POSTGRES,
@@ -18,4 +21,5 @@ DB_CREDENTIALS = \
     f"{DATABASE_POSTGRES}"
 
 async_engine = create_async_engine(DB_CREDENTIALS, echo=True)
+async_session = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
--- app/core/database/utils.py
+++ app/core/database/utils.py
@@ -1,15 +1,11 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import sessionmaker
-
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 
 from app.core.database.models import User
-from app.core.database.init import async_engine
+from app.core.database.init import async_session, AsyncSession
 
 
 async def get_session() -> AsyncSession:
-    async_session = sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         yield session
 
