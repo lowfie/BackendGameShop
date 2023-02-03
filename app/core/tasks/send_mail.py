a7a736937d8d95828510da039dffd67a26cd082b
Andrew Veselov <vesell4123@mail.ru>
2023-02-03 13:45:32 +0300

    change description mail message

--- app/core/tasks/send_mail.py
+++ app/core/tasks/send_mail.py
@@ -15,21 +15,22 @@ from app.settings.config import (
 celery = Celery('tasks', broker=f'redis://{HOST_REDIS}:{PORT_REDIS}')
 
 
-def get_email_game_discount_info(to_addr: str, game_data: dict) -> EmailMessage:
+def get_email_game_discount_info(game_data: dict, to_addr: str) -> EmailMessage:
     email = EmailMessage()
     email['Subject'] = 'GameShopApi: discount notification'
     email['From'] = SMTP_MAIL
-    email['To'] = SMTP_MAIL
+    email['To'] = to_addr
     email.set_content(
         'В вашей корзине появилась игра со скидкой!\n'
-        ''
+        f'Скидка на игру {game_data["title"]} в {game_data["discount"]*100}%, теперь она стоит {game_data["price"]} рублей\n'
+        'Успей купить!',
     )
     return email
 
 
 @celery.task
-def send_mail_game_discount(user_mail: str, game_data: dict):
-    email = get_email_game_discount_info(user_mail, game_data)
+def send_mail_game_discount(game_data: dict, to_email: str):
+    email = get_email_game_discount_info(game_data, to_email)
     with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
         server.login(SMTP_MAIL, SMTP_PASSWORD)
-        server.sendmail(email)
+        server.send_message(email)
